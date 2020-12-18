fc3dd12c85b8b7e02cccf507e995c9d308322761
jmgomez2018 <jmgametal@gmail.com>
2020-12-18 09:16:57 -0300

    Se finaliza la seccion 03

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public isAddTimerVisible:boolean = false;
+  public time:number = 0;
 
   constructor() { }
+
+  logCountdownEnd() {
+    console.log('The Countdown Has Finished!!!')
+  }
+
+  public showAddTimer(){
+    this.isAddTimerVisible = true;
+  }
+
+  public hideAddTimer(){
+    this.isAddTimerVisible = false;
+  }
 
 }
